4f3ee46ca7947795a23a2ff90667805ef7bf92ca
Jonathan Capps <cappsy@gmail.com>
2025-04-06 23:43:36 +0100

    zed, starship, pipewire libs

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,8 +11,6 @@ dnf5 -y copr enable ublue-os/packages
 dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 -y copr enable gmaglione/podman-bootc
 dnf5 -y copr enable che/nerd-fonts
-dnf5 -y copr enable atim/ubuntu-fonts
-sh <(curl https://terra.fyralabs.com/get.sh)
 
 ### COSMIC
 dnf5 -y install @cosmic-desktop @cosmic-desktop-apps
@@ -28,16 +26,15 @@ done < "/ctx/packages.txt"
 ### Manual installs
 
 # Zed editor
-# extract zed to ~/.local/zed.app/
-#tar -xvf https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz -C /tmp
-# link the zed binary to ~/.local/bin (or another directory in your $PATH)
-#ln -sf ~/.local/zed.app/bin/zed ~/.local/bin/zed
+curl --retry 3 -Lo /tmp/zed-linux-x86_64.tar.gz "https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz"
+tar -xvf /tmp/zed-linux-x86_64.tar.gz -C /usr
+ln -sf /usr/zed.app/bin/zed /usr/bin/zed
 
 # Starship Shell Prompt
-#curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-#tar -xzf /tmp/starship.tar.gz -C /tmp
-#install -c -m 0755 /tmp/starship /usr/bin
-#echo 'eval "$(starship init bash)"' >> /etc/bashrc
+curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
 ### Disable repos
 dnf5 -y copr disable ublue-os/staging
@@ -45,7 +42,6 @@ dnf5 -y copr disable ublue-os/packages
 dnf5 -y copr disable ryanabx/cosmic-epoch
 dnf5 -y copr disable gmaglione/podman-bootc
 dnf5 -y copr disable che/nerd-fonts
-dnf5 -y copr disable atim/ubuntu-fonts
 
 ### Clean-up
 dnf5 clean all
