52c37187a07bac6546f8df721fcc1da360024bb7
Jonathan Capps <jonathan@eventogy.com>
2025-04-11 20:25:49 +0100

    script setup

--- build_scripts/01-files.sh
+++ build_scripts/01-files.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
 ### System files
 cp -avf "/ctx/files/." /
--- build_scripts/03-packages.sh
+++ build_scripts/03-packages.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
 ### Enable repos
 dnf5 -y copr enable ublue-os/staging
 dnf5 -y copr enable ublue-os/packages
