6742a81fe70202889d6264eb1bbe9f5e4e8a653d
Jonathan Capps <jonathan@eventogy.com>
2025-04-11 20:58:29 +0100

    script setup

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -3,7 +3,10 @@
 set -ouex pipefail
 
 ### Run sub-scripts
-"${realpath /ctx/build_scripts/01-start.sh}"
-"${realpath /ctx/build_scripts/02-info.sh}"
-"${realpath /ctx/build_scripts/03-packages.sh}"
-"${realpath /ctx/build_scripts/04-end.sh}"
+find "/ctx/build_scripts/" -maxdepth 1 -iname "*-*.sh" -type f -print0 | sort --zero-terminated --sort=human-numeric | while IFS= read -r -d $'\0' script ; do
+	"$(sh $script)"
+done
+#"$(realpath ../ctx/build_scripts/01-start.sh)"
+#"$(realpath ../ctx/build_scripts/02-info.sh)"
+#"$(realpath ../ctx/build_scripts/03-packages.sh)"
+#"$(realpath ../ctx/build_scripts/04-end.sh)"
