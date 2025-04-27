84ada374fb9974e3513edfff1f6c4e015ee24d84
Jonathan Capps <cappsy@gmail.com>
2025-04-27 13:20:25 +0100

    dx image

--- build_scripts/03-packages.sh
+++ build_scripts/03-packages.sh
@@ -16,15 +16,15 @@ if [[ $IMAGE_FLAVOUR == "-dx" ]]; then
     curl --retry 3 -Lo /tmp/zed-linux-x86_64.tar.gz "https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz"
     tar -xvf /tmp/zed-linux-x86_64.tar.gz -C /usr
     ln -sf /usr/zed.app/bin/zed /usr/bin/zed
+
+    # Zed editor .desktop files
+    cp /usr/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
+    sed -i "s|Icon=zed|Icon=/usr/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /usr/share/applications/dev.zed.Zed.desktop
+    sed -i "s|Exec=zed|Exec=/usr/zed.app/libexec/zed-editor|g" /usr/share/applications/dev.zed.Zed.desktop
+else
+    # Starship Shell Prompt
+    curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xzf /tmp/starship.tar.gz -C /tmp
+    install -c -m 0755 /tmp/starship /usr/bin
+    echo 'eval "$(starship init bash)"' >> /etc/bashrc
 fi
-
-# Zed editor .desktop files
-cp /usr/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s|Icon=zed|Icon=/usr/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s|Exec=zed|Exec=/usr/zed.app/libexec/zed-editor|g" /usr/share/applications/dev.zed.Zed.desktop
-
-# Starship Shell Prompt
-curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-tar -xzf /tmp/starship.tar.gz -C /tmp
-install -c -m 0755 /tmp/starship /usr/bin
-echo 'eval "$(starship init bash)"' >> /etc/bashrc
