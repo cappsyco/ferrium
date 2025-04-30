05dd3ca44c6b28dcdeec42f3efedb3a87c9913fc
Jonathan Capps <cappsy@gmail.com>
2025-04-30 22:16:54 +0100

    trying to fix flatpaks

--- build_scripts/04-end.sh
+++ build_scripts/04-end.sh
@@ -12,23 +12,23 @@ dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable ublue-os/packages
 dnf5 -y copr disable che/nerd-fonts
 
-### Flatpaks
-mkdir -p /etc/flatpak/remotes.d
-curl --retry 3 -o /etc/flatpak/remotes.d/flathub.flatpakrepo "https://dl.flathub.org/repo/flathub.flatpakrepo"
-systemctl disable flatpak-add-fedora-repos.service
-
 ### Services
 systemctl --global preset-all
+systemctl --global enable ublue-flatpak-manager.service
 systemctl enable sshd
 if [[ $ENABLE_DX == "1" ]]; then
     systemctl enable podman
     systemctl enable docker
 fi
 
+### Flatpaks
+flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+systemctl disable flatpak-add-fedora-repos.service
+
 ### Clean-up
 dnf5 clean all
 
-### Dracut actions
+### Initramfs rebuild
 echo "add_dracutmodules+=\" resume \"" >/etc/dracut.conf.d/resume.conf
 KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//' | tail -n 1)"
