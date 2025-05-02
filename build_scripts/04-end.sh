d2626fc794d5999e938426f60dd4207c8321fd48
Jonathan Capps <cappsy@gmail.com>
2025-05-02 20:48:10 +0100

    cleanup

--- build_scripts/04-end.sh
+++ build_scripts/04-end.sh
@@ -15,7 +15,6 @@ dnf5 -y copr disable che/nerd-fonts
 ### Services
 systemctl --global preset-all
 systemctl enable sshd.service
-systemctl enable check-sb-key.service
 
 if [[ $ENABLE_DX == "1" ]]; then
     systemctl enable ferrium-dx-groups.service
